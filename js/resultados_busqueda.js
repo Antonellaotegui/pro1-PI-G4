e9adcea8185851cb4efe93fc479cfdfb92288ff1
antonellaotegui <aotegui@udesa.edu.ar>
2022-11-16 16:24:18 -0300

    busqueda pero no nos linkea la api

--- js/resultados_busqueda.js
+++ js/resultados_busqueda.js
@@ -11,24 +11,24 @@ fetch(url)
 ).then(function(data) {
      //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
      console.log(data);
-     let arrayDePersonajes = data.results;
+     let arrayPeliculas = data.results;
 
      //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
      let seccion = document.querySelector('.container');
-     let allCharacters = [];
+     let allPeliculas = [];
 
-     console.log(arrayDePersonajes);
+     console.log(arrayPeliculas);
      //2 Qué: recorro la información de la api y la organizo para mostarla en el html
-     for(let i=0; i<arrayDePersonajes.length; i++){
+     for(let i=0; i<arrayPeliculas.length; i++){
          //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
-         allCharacters += `<a href="./detalle.html?buscador=${arrayDePersonajes[i].id}"><article>
-                             <img src=${arrayDePersonajes[i].image} alt='${arrayDePersonajes[i].name}' />
-                             <p>Name: ${arrayDePersonajes[i].name} </p>
-                             <p>Status: ${arrayDePersonajes[i].status} </p>
+         allPeliculas += `<a href="./detalle.html?buscador=${arrayPeliculas[i].id}"><article>
+                             <img src=${arrayPeliculas[i].image} alt='${arrayPeliculas[i].name}' />
+                             <p>Name: ${arrayPeliculas[i].name} </p>
+                             <p>Status: ${arrayPeliculas[i].status} </p>
                          </article></a>`
      }
      //Con toda la estructura html completa ahora la paso al DOM
-     seccion.innerHTML = allCharacters;
+     seccion.innerHTML = allPeliculas;
     return data;
 }
 ).catch(function(error) {
