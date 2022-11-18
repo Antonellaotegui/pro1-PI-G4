b6971962bc510a21609e665afad378c67ca351f2
ariana-souza <asouza@udesa.edu.ar>
2022-11-18 16:10:29 -0300

    detail genres

--- js/detail_genres.js
+++ js/detail_genres.js
@@ -1,3 +1,33 @@
 let api_key = "81faef6942a31915ed87b416fbba64ba";
 let url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`; 
 
+fetch(url)
+.then(function(response) {
+    return response.json();
+}
+).then(function(data) {
+     //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
+     console.log(data);
+
+     //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
+     let seccion = document.querySelector('#container');
+     let allDetailg = [];
+
+     console.log(arrayPeliculas);
+     //2 Qué: recorro la información de la api y la organizo para mostarla en el html
+     for(let i=0; i<arrayPeliculas.length; i++){
+         //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
+         allDetailg += ``
+     }
+     //Con toda la estructura html completa ahora la paso al DOM
+     seccion.innerHTML = allDetailg;
+    return data;
+}
+).catch(function(error) {
+    return error;
+}
+);
+
+
+
+
