b31af81f4af26a07b2d246463c8930ee1e91ef2a
ariana-souza <asouza@udesa.edu.ar>
2022-11-18 14:37:49 -0300

    detail genres api

---
+++ js/detail_genres.js
@@ -0,0 +1,3 @@
+let api_key = "81faef6942a31915ed87b416fbba64ba";
+let url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`; 
+ 
