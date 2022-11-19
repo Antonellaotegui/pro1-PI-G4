fa1ef12fa850d4b166571a12c288bdbae470c8e7
justinaalric <jalricferre@udesa.edu.ar>
2022-11-19 15:13:10 -0300

    agregar series a favoritos hecho, pero se me agregan de más

--- js/favorite.js
+++ js/favorite.js
@@ -30,7 +30,7 @@ if (favoritos == null || favoritos.length == 0) {
                                         <a href="./detail-movies.html">
                                         <img  class= "pelis" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="'${data.title}'">
                                         </a>
-                                        <p class="titulo"> <a href="./detallePersonaje.html?idPersonaje=${data.id}"> ${data.title}</a></p>
+                                        <p class="titulo"> <a href="./detail-movie.html?idPelicula=${data.id}"> ${data.title}</a></p>
                                         <p class="estreno"> ${data.release_date}</p>
                                     </article>`
             section.innerHTML = peliculasFavoritos;
@@ -38,9 +38,30 @@ if (favoritos == null || favoritos.length == 0) {
             return data;
         }).catch(function (error) {
             return error;
-        });
+        }); 
+        
+        let urlSerie  = `https://api.themoviedb.org/3/tv/${favoritos[i]}?api_key=${api_key}&language=en-US`;
+            fetch(urlSerie)
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+                peliculasFavoritos += `<article class="cajas">
+                                            <a href="./detail-movies.html">
+                                            <img  class= "pelis" src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="'${data.name}'">
+                                            </a>
+                                            <p class="titulo"> <a href="./detail-movie.html?idPelicula=${data.id}"> ${data.name}</a></p>
+                                            <p class="estreno"> ${data.first_air_date}</p>
+                                        </article>`
+                section.innerHTML = peliculasFavoritos;
 
-        
-        
-    }
-}
+                return data;
+            }).catch(function (error) {
+                return error;
+            }); 
+            
+        }}
+
+
+    
+    
