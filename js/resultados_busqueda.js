ca42276b376b3ad373043778f36c431bab7f8437
justinaalric <jalricferre@udesa.edu.ar>
2022-11-19 19:53:30 -0300

    tratando de solucionar el forms

--- js/resultados_busqueda.js
+++ js/resultados_busqueda.js
@@ -9,11 +9,12 @@
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 const query = queryStringObj.get('buscador');
+let api_key = '81faef6942a31915ed87b416fbba64ba';
 
-const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=45d43a6901861343cdb188d4f3bafd7c&language=en-US&page=1&include_adult=false`
+const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${api_key}&language=en-US&page=1&include_adult=false`
 
-let loquebusco= document.querySelector('.loquebusco');
-  loquebusco.innerText= query;
+//let loquebusco= document.querySelector('.loquebusco');
+  //loquebusco.innerText= query;
 
 fetch(url)
 .then(function(response) {
@@ -33,26 +34,21 @@ fetch(url)
      if (allPeliculas == null || allPeliculas.length == 0) {
         /* Muestres no hay resultados */
         seccion.innerHTML = '<p>No hay resultado de busqueda</p>'}
-
-    
-        else { for(let i=0; i<arrayPeliculas.length; i++){
-         //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
+    else { 
+        for(let i=0; i<arrayPeliculas.length; i++){
+         //Dentro del for voy acumulando en la variable una estructura html por cada pelicula del array.
          allPeliculas += `<a href="./detalle.html?buscador=${arrayPeliculas[i].id}"><article class="cajas">
                              <img class="pelis" src=https://image.tmdb.org/t/p/w500/${arrayPeliculas[i].poster_path} alt='${arrayPeliculas[i].title}' />
                              <p class="titulo" >${arrayPeliculas[i].title} </p>
                              <p class="estreno" >${arrayPeliculas[i].release_date} </p>
                          </article></a>`
+        
      }
+        
+    }
      //Con toda la estructura html completa ahora la paso al DOM
-<<<<<<< HEAD
      seccion.innerHTML = allPeliculas;
-    
-    
-=======
-     seccion.innerHTML = allPeliculas;
-     
->>>>>>> 407a809507a3a5f9b0fc3d3b902fc8d77a71d036
-    return data;
+        return data;
 }
 ).catch(function(error) {
     return error;
