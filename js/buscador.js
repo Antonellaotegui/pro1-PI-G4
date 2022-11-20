13391b2b55b57459d3f49c4f882be46adb44d4a7
justinaalric <jalricferre@udesa.edu.ar>
2022-11-20 16:35:05 -0300

    cambios

--- js/buscador.js
+++ js/buscador.js
@@ -5,8 +5,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     if ( input.value == '') {
         alert('Debes Ingresar una palabra');
-    } else if( input.value.length < 5){
-        alert('Palabra muy corta, deben ser 5 letras');
+    } else if( input.value.length < 2){
+        alert('Palabra muy corta, deben ser 3 letras o más');
     }else {
         form.submit();
     }
